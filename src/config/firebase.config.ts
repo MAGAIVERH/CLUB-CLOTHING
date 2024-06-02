607ec40bc6e36fbf6c139ccc96065c667118532f
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-02 17:47:03 -0300

    chore: add fire configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEAB4s96FURm2B5DpCVzbng6oaF-4EX_E',
+  authDomain: 'club-ecommerce-clothes.firebaseapp.com',
+  projectId: 'club-ecommerce-clothes',
+  storageBucket: 'club-ecommerce-clothes.appspot.com',
+  messagingSenderId: '62136121407',
+  appId: '1:62136121407:web:560211a3158e8c3ef5a08a'
+}
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
