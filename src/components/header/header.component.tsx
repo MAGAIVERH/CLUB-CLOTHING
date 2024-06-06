605a6f3df2a89ba5334db3484ef4d6c461afe8bf
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-06 19:49:13 -0300

    feat: add initial Login Page and Login Route

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,4 +1,5 @@
 import { BsCart3 } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 
 // Styles
 import {
@@ -9,13 +10,18 @@ import {
 } from './header.styles'
 
 const Header = () => {
+  const navigate = useNavigate()
+  const handleLoginClick = () => {
+    navigate('/login')
+  }
+
   return (
     <HeaderConatiner>
       <HeaderTitle>CLUB CLOTHING</HeaderTitle>
 
       <HeaderItems>
         <HeaderItem>Explorar</HeaderItem>
-        <HeaderItem>Login</HeaderItem>
+        <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
         <HeaderItem>Criar Conta</HeaderItem>
         <HeaderItem>
           <BsCart3 size={25} />
