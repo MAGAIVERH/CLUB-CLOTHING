6f414bab61f91db66e8a4e9bf52811a3d98c7ae5
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-06-11 09:58:22 -0300

    feat: add user account creation

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -1,6 +1,8 @@
 import { FiLogIn } from 'react-icons/fi'
 import { useForm } from 'react-hook-form'
 import validator from 'validator'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
 
 // Componenets
 import CustomButton from '../../components/custom-button/custom-button-component'
@@ -16,8 +18,11 @@ import {
   SignUpInputContainer
 } from './sign-up.styles'
 
+// Utilities
+import { auth, db } from '../../config/firebase.config'
+
 interface SignUpForm {
-  name: string
+  firstName: string
   lastname: string
   email: string
   password: string
@@ -34,8 +39,23 @@ const SignUpPage = () => {
 
   const watchPassword = watch('password')
 
-  const handleSubmitPress = (data: SignUpForm) => {
-    console.log({ data })
+  const handleSubmitPress = async (data: SignUpForm) => {
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+
+      await addDoc(collection(db, 'user'), {
+        id: userCredentials.user.uid,
+        email: userCredentials.user.email,
+        firstName: data.firstName,
+        lastName: data.lastname
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -49,12 +69,12 @@ const SignUpPage = () => {
           <SignUpInputContainer>
             <p>Nome</p>
             <CustomInput
-              hasError={!!errors?.name}
+              hasError={!!errors?.firstName}
               placeholder='Digite seu nome'
-              {...register('name', { required: true })}
+              {...register('firstName', { required: true })}
             />
 
-            {errors?.name?.type === 'required' && (
+            {errors?.firstName?.type === 'required' && (
               <InputErrorMessage>O nome é obrigatório.</InputErrorMessage>
             )}
           </SignUpInputContainer>
