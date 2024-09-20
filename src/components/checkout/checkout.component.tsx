8d481e083a0266c42a41f3e86d40fbc008aec9f2
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-09-19 22:06:16 -0300

    fix: investigate issue with API environment variable configuration

--- src/components/checkout/checkout.component.tsx
+++ src/components/checkout/checkout.component.tsx
@@ -1,71 +1,75 @@
-import { FunctionComponent, useContext, useState } from 'react'
-import { BsBagCheck } from 'react-icons/bs'
-import axios from 'axios'
+import { FunctionComponent, useContext, useState } from 'react';
+import { BsBagCheck } from 'react-icons/bs';
+import axios from 'axios';
 
 // Utilities
-import { CartContext } from '../../contexts/cart.context'
+import { CartContext } from '../../contexts/cart.context';
 
 // Styles
 import {
-  CheckoutContainer,
-  CheckoutTitle,
-  CheckoutProducts,
-  CheckoutTotal
-} from './checkout.styles'
-import CustomButton from '../custom-button/custom-button-component'
-import CartItem from '../cart-item/cart-item.component'
-import Loading from '../loading/loading.component'
+    CheckoutContainer,
+    CheckoutTitle,
+    CheckoutProducts,
+    CheckoutTotal,
+} from './checkout.styles';
+import CustomButton from '../custom-button/custom-button-component';
+import CartItem from '../cart-item/cart-item.component';
+import Loading from '../loading/loading.component';
 
 const Checkout: FunctionComponent = () => {
-  const { products, productsTotalPrice } = useContext(CartContext)
+    const { products, productsTotalPrice } = useContext(CartContext);
 
-  const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleFinishPurchaseClick = async () => {
-    try {
-      setIsLoading(true)
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_API_URL}/create-checkout-session`,
-        {
-          products
+    const handleFinishPurchaseClick = async () => {
+        try {
+            setIsLoading(true);
+            console.log(
+                'Calling API at:',
+                `${process.env.REACT_APP_API_URL}/create-checkout-session`,
+            );
+            const { data } = await axios.post(
+                `${process.env.REACT_APP_API_URL}/create-checkout-session`,
+                {
+                    products,
+                },
+            );
+
+            window.location.href = data.url;
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
         }
-      )
+    };
 
-      window.location.href = data.url
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
+    return (
+        <CheckoutContainer>
+            {isLoading && <Loading />}
+            <CheckoutTitle>Checkout</CheckoutTitle>
 
-  return (
-    <CheckoutContainer>
-      {isLoading && <Loading />}
-      <CheckoutTitle>Checkout</CheckoutTitle>
+            {products.length > 0 ? (
+                <>
+                    <CheckoutProducts>
+                        {products.map((product) => (
+                            <CartItem key={product.id} product={product} />
+                        ))}
+                    </CheckoutProducts>
 
-      {products.length > 0 ? (
-        <>
-          <CheckoutProducts>
-            {products.map((product) => (
-              <CartItem key={product.id} product={product} />
-            ))}
-          </CheckoutProducts>
+                    <CheckoutTotal>Total: R${productsTotalPrice}</CheckoutTotal>
 
-          <CheckoutTotal>Total: R${productsTotalPrice}</CheckoutTotal>
+                    <CustomButton
+                        startIcon={<BsBagCheck />}
+                        onClick={handleFinishPurchaseClick}
+                    >
+                        Finalizar Compras
+                    </CustomButton>
+                </>
+            ) : (
+                <p>Seu Carrinho está vazio!</p>
+            )}
+        </CheckoutContainer>
+    );
+};
 
-          <CustomButton
-            startIcon={<BsBagCheck />}
-            onClick={handleFinishPurchaseClick}
-          >
-            Finalizar Compras
-          </CustomButton>
-        </>
-      ) : (
-        <p>Seu Carrinho está vazio!</p>
-      )}
-    </CheckoutContainer>
-  )
-}
-
-export default Checkout
+export default Checkout;
